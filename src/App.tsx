4ce8f9512becf61a3d71a762fe8d0a6da63f308d
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2024-12-05 16:42:29 +0530

    add todo input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-let name: number | string = 10;
-name = '19';
-console.log(name);
+import { useState } from 'react';
+import AddTask from './components/AddTask';
 
-function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
-}
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<string[]>([]);
+
+  console.log(todos);
+  return (
+    <div className=' bg-purple-950 h-screen p-4 '>
+      <div className=' text-center space-y-2 p-4 uppercase tracking-wide'>
+        <h1 className='text-3xl font-bold text-yellow-200'>todo app</h1>
+        <h3 className=' text-xl text-purple-300 font-medium tracking-widest'>
+          using typescript
+        </h3>
+      </div>
+
+      <div className=' pt-4 space-y-4'>
+        <h2 className=' capitalize text-lg text-white font-medium'>
+          add new task
+        </h2>
+        <AddTask setTodos={setTodos} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/AddTask.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+interface PropTypes {
+  setTodos: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+export default function AddTask({ setTodos }: PropTypes) {
+  const [task, setTask] = useState<string>('');
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    if (task.trim() !== '') {
+      setTodos((prev) => [...prev, task.trim()]);
+      setTask('');
+    }
+  }
+
+  return (
+    <form className=' flex' onSubmit={(e) => handleSubmit(e)}>
+      <input
+        type='text'
+        placeholder='name a task'
+        className=' px-4 py-2 text-lg rounded-l-md w-full outline-none bg-purple-100 focus:bg-white placeholder:capitalize transition-all duration-200'
+        onChange={(e) => setTask(e.target.value)}
+        value={task}
+      />
+      <button className=' capitalize px-12 py-2 font-semibold  text-lg text-yellow-800 hover:text-orange-700 rounded-r-md bg-yellow-300 hover:bg-yellow-400 transition-all duration-200'>
+        add
+      </button>
+    </form>
+  );
+}
