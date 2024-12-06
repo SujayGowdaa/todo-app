081060d0a295f7b4ce542d46e760288f0ed05d70
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2024-12-06 16:32:06 +0530

    add no list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import { useState } from 'react';
-import AddTask from './components/AddTask';
 import { Todo } from './model';
+import AddTask from './components/AddTask';
+import TodoList from './components/TodoList';
+import NoList from './components/NoList';
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   console.log(todos);
   return (
-    <div className=' bg-purple-950 h-screen p-12 space-y-12'>
+    <div className=' flex flex-col gap-12 bg-violet-950 h-screen p-12'>
       <div className=' flex gap-4 space-y-2 uppercase tracking-wide'>
-        <h1 className='text-3xl font-bold text-yellow-200'>todo app »</h1>
-        <h3 className=' text-lg text-purple-300 font-medium tracking-[8px]'>
+        <h1 className='text-3xl font-bold text-orange-200'>todo app »</h1>
+        <h3 className=' text-lg text-violet-300 font-medium tracking-[8px]'>
           using typescript
         </h3>
       </div>
       <div className='space-y-4'>
-        <h2 className=' capitalize text-lg text-purple-200 font-medium'>
+        <h2 className=' capitalize text-lg text-violet-200 font-medium'>
           add new task
         </h2>
         <AddTask setTodos={setTodos} />
       </div>
+
+      {todos.length > 0 ? (
+        <ul className=' grid gap-8'>
+          {todos.map((todo, i) => (
+            <TodoList data={todo} i={i + 1} />
+          ))}
+        </ul>
+      ) : (
+        <NoList />
+      )}
     </div>
   );
 };
---
+++ src/components/NoList.tsx
@@ -0,0 +1,7 @@
+export default function NoList() {
+  return (
+    <div className=' flex grow bg-violet-900 items-center justify-center text-violet-100 shadow-lg'>
+      <p className=' text-lg font-medium'>No list available...</p>
+    </div>
+  );
+}
