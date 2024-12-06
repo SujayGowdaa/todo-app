c814feb1346bffc015644a6c5291cdfb6a2219ab
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2024-12-06 11:57:22 +0530

    add todo model

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
+import { Todo } from '../model';
+import { v4 as generateId } from 'uuid';
 
 interface PropTypes {
-  setTodos: React.Dispatch<React.SetStateAction<string[]>>;
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
 export default function AddTask({ setTodos }: PropTypes) {
-  const [task, setTask] = useState<string>('');
+  const [todo, setTodo] = useState<string>('');
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    if (task.trim() !== '') {
-      setTodos((prev) => [...prev, task.trim()]);
-      setTask('');
+    if (todo.trim() !== '') {
+      setTodos((prev) => [
+        ...prev,
+        { id: generateId(), todo: todo.trim(), isDone: false },
+      ]);
+      setTodo('');
     }
   }
 
@@ -19,10 +24,10 @@ export default function AddTask({ setTodos }: PropTypes) {
     <form className=' flex' onSubmit={(e) => handleSubmit(e)}>
       <input
         type='text'
-        placeholder='Enter task'
+        placeholder='Enter todo'
         className=' px-4 py-2 text-lg rounded-l-md w-full outline-none bg-purple-100 focus:bg-white transition-all duration-200'
-        onChange={(e) => setTask(e.target.value)}
-        value={task}
+        onChange={(e) => setTodo(e.target.value)}
+        value={todo}
       />
       <button className=' capitalize px-12 py-2 font-semibold  text-lg text-yellow-800 hover:text-orange-700 rounded-r-md bg-yellow-300 hover:bg-yellow-400 transition-all duration-200'>
         add
